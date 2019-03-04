17245270f829f58b3d74beebd41636974363aedf
Mike <mmagliar@its.jnj.com>
2019-03-03 22:31:41 -0500

    number2

--- python2/mainprogram.py
+++ python2/mainprogram.py
@@ -1,39 +1,61 @@
-#analyzing the financial records of your company.
+#analyzing the election date
 import os
 import csv
-
-month = 0
-monthdollar2 = 0
-previous = 0
-current = 0
-variance = 0
-ytdvariance = 0
-prev_line = None
-
 #import csv file
 csvpath = os.path.join('resource', 'election_data.csv')
+
+electionresults = { }
+totalvotes = 0  
 
 with open(csvpath, newline='') as csvfile:
     
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    print(csvreader)
+    #print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-      
-    
+    #print(f"CSV Header: {csv_header}")
+   
 # Read each row of data after the header
     for row in csvreader:
-            #print(row)    
-           
-            month = month + 1
-            monthdollar = (int(row[0]))
-            monthdollar2 = monthdollar + monthdollar2 
-                    
-            previous = current
-            current = monthdollar 
-            variance = current - previous
-            ytdvariance =  (variance + ytdvariance)
-            percent = ((ytdvariance) / (month))
+        totalvotes += 1    
+        if row[2] not in electionresults:
+            electionresults[row[2]]=1
+        else:
+            electionresults[row[2]]+=1  
+
+candidatevotes = list(electionresults.keys())     
+results = list(electionresults.values())
+
+print(candidatevotes)
+print(electionresults)
+print(results)
+
+print("Election Results")
+print("------------------------------")
+print(f"Total Votes: {'{:,.0f}'.format(totalvotes)}")
+print("------------------------------")
+#print(f"{candidatevotes[0]}: {'{:,.0f}'.format(electionresults[0])}")
+#print((candidatevotes[0]), (electionresults[0]))
+print(f"{candidatevotes[0]} {(electionresults[0]/totalvotes*100)} {(electionresults[0])}")
+#print(f"{candidatevotes[0]}: {'{:,.0f}'.format(electionresults[0]/totalvotes*100)}% ({'{:,.0f}'.format(electionresults[0])})")
+# print(f"{candidatevotes[1]}: {'{:,.0f}'.format(electionresults[1]/totalvotes*100)}% ({'{:,.0f}'.format(electionresults[1])})")
+# print(f"{candidatevotes[2]}: {'{:,.0f}'.format(electionresults[2]/totalvotes*100)}% ({'{:,.0f}'.format(electionresults[2])})")
+# print(f"{candidatevotes[3]}: {'{:,.0f}'.format(electionresults[3]/totalvotes*100)}% ({'{:,.0f}'.format(electionresults[3])})")
+print(f"Winner: {results[0]}")
+
+
+# output_path = os.path.join('resource', 'output_data.csv')
+# with open(output_path, 'w', newline='') as csvfile:
+   
+#     csvwriter = csv.writer(csvfile)
+#     csvwriter.writerow(['Election Results'])
+#     csvwriter.writerow(['----------------------'])
+#     csvwriter.writerow([f"Total Votes: {'{:,.0f}'.format(totalvotes)}"])
+#     csvwriter.writerow(['----------------------'])
+#     csvwriter.writerow([f"Total Dollars:  {'${:,.2f}'.format(sum(totaldollars))}\n"])
+#     csvwriter.writerow([f"Average Change:  {'${:,.2f}'.format(round(sum(monthlychange)/len(monthlychange),2))}\n"])
+#     csvwriter.writerow([f"Greatest Increase in Profits: {totalmonths[maxgainmonth]} {str('${:,.2f}'.format(maxincrease))}\n"])
+#     csvwriter.writerow([f"Greatest Decrease in Profits: {totalmonths[minlossmonth]} {str('${:,.2f}'.format(maxloss))}\n"])
+#     csvwriter.writerow(['---End of Analysis---'])
